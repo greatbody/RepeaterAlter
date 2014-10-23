22fee723c44df4579b18b55ea6ed003b8edca7cf
greatbody <wedown@163.com>
2014-10-24 00:47:59 +0800

    sign in base codes

---
+++ RepeatorAlter/Form1.Designer.vb
@@ -0,0 +1,174 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.txtCode = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtField = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.lstDest = New System.Windows.Forms.ListBox()
+        Me.lstSource = New System.Windows.Forms.ListBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtDestCode = New System.Windows.Forms.TextBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.btnAdd)
+        Me.GroupBox1.Controls.Add(Me.txtCode)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.txtField)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(8, 9)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(849, 183)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "代码输入"
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(236, 15)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(66, 28)
+        Me.btnAdd.TabIndex = 4
+        Me.btnAdd.Text = "新增"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'txtCode
+        '
+        Me.txtCode.Location = New System.Drawing.Point(39, 54)
+        Me.txtCode.Multiline = True
+        Me.txtCode.Name = "txtCode"
+        Me.txtCode.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtCode.Size = New System.Drawing.Size(804, 122)
+        Me.txtCode.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(4, 52)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(29, 12)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "代码"
+        '
+        'txtField
+        '
+        Me.txtField.Location = New System.Drawing.Point(39, 20)
+        Me.txtField.Name = "txtField"
+        Me.txtField.Size = New System.Drawing.Size(187, 21)
+        Me.txtField.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(4, 23)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(29, 12)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "字段"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.lstDest)
+        Me.GroupBox2.Controls.Add(Me.lstSource)
+        Me.GroupBox2.Location = New System.Drawing.Point(8, 198)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(321, 203)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "选择区"
+        '
+        'lstDest
+        '
+        Me.lstDest.FormattingEnabled = True
+        Me.lstDest.ItemHeight = 12
+        Me.lstDest.Location = New System.Drawing.Point(168, 20)
+        Me.lstDest.Name = "lstDest"
+        Me.lstDest.Size = New System.Drawing.Size(147, 172)
+        Me.lstDest.TabIndex = 1
+        '
+        'lstSource
+        '
+        Me.lstSource.FormattingEnabled = True
+        Me.lstSource.ItemHeight = 12
+        Me.lstSource.Location = New System.Drawing.Point(8, 20)
+        Me.lstSource.Name = "lstSource"
+        Me.lstSource.Size = New System.Drawing.Size(147, 172)
+        Me.lstSource.TabIndex = 0
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(335, 198)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(53, 12)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "生成代码"
+        '
+        'txtDestCode
+        '
+        Me.txtDestCode.Location = New System.Drawing.Point(342, 217)
+        Me.txtDestCode.Multiline = True
+        Me.txtDestCode.Name = "txtDestCode"
+        Me.txtDestCode.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtDestCode.Size = New System.Drawing.Size(514, 172)
+        Me.txtDestCode.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(869, 403)
+        Me.Controls.Add(Me.txtDestCode)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Repeater 列修改工具   【SunSoft · 成渝一组】"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents btnAdd As System.Windows.Forms.Button
+    Friend WithEvents txtCode As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtField As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents lstDest As System.Windows.Forms.ListBox
+    Friend WithEvents lstSource As System.Windows.Forms.ListBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txtDestCode As System.Windows.Forms.TextBox
+
+End Class
---
+++ RepeatorAlter/Form1.vb
@@ -0,0 +1,56 @@
+﻿Imports System.Text
+
+Public Class Form1
+    Private _fieldCode As New Dictionary(Of String, String)
+
+    Private Sub btnAdd_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnAdd.Click
+        If _fieldCode.ContainsKey(txtField.Text) Then
+            If MsgBox("字段名重复，是否覆盖原内容？", MsgBoxStyle.YesNo) = MsgBoxResult.Yes Then
+                '覆盖原内容
+                _fieldCode.Item(txtField.Text) = txtCode.Text
+            Else
+                txtField.SelectAll()
+                txtField.Focus()
+                Exit Sub
+            End If
+        Else
+            '第一次添加
+            _fieldCode.Add(txtField.Text, txtCode.Text)
+            lstSource.Items.Add(txtField.Text)
+        End If
+        '清空
+        txtField.Clear()
+        txtCode.Clear()
+        '聚焦
+        txtField.Focus()
+    End Sub
+
+    Private Sub lstSource_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles lstSource.Click
+        If lstSource.Items.Count > 0 AndAlso lstSource.SelectedIndex >= 0 Then
+            lstDest.Items.Add(lstSource.Items(lstSource.SelectedIndex).ToString())
+            lstSource.Items.RemoveAt(lstSource.SelectedIndex)
+        End If
+        txtDestCode.Clear()
+        txtDestCode.AppendText(CreateCode())
+    End Sub
+
+    Private Sub lstDest_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles lstDest.Click
+        If lstDest.Items.Count > 0 AndAlso lstDest.SelectedIndex >= 0 Then
+            lstSource.Items.Add(lstDest.Items(lstDest.SelectedIndex).ToString())
+            lstDest.Items.RemoveAt(lstDest.SelectedIndex)
+        End If
+        txtDestCode.Clear()
+        txtDestCode.AppendText(CreateCode())
+    End Sub
+
+    Public Function CreateCode() As String
+        Dim htmlCode As New StringBuilder
+        If lstDest.Items.Count > 0 Then
+            For i As Integer = 0 To lstDest.Items.Count - 1
+                htmlCode.AppendLine(_fieldCode.Item(lstDest.Items(i)))
+            Next
+        End If
+
+        Return htmlCode.ToString()
+    End Function
+End Class
